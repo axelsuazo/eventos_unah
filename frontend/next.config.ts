a3106a656595febbb8119b07acdb27f7530d5d0b
axelsuazo <axelsuazojr@unitec.edu>
2026-06-02 14:11:32 -0600

    configuration Vercel

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
 import type { NextConfig } from "next";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const nextConfig: NextConfig = {
   output: process.env.NEXT_OUTPUT_MODE === "export" ? "export" : undefined,
+  turbopack: {
+    root: __dirname, // Establece la raíz de Turbopack a la carpeta del frontend
+  },
 };
 
 export default nextConfig;
